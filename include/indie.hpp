7b480da05f59051c31862e6d5620d45c950c40c4
0xArKrant <contact@arkrant.us>
2021-05-12 19:26:51 +0200

    [ADD] ✨ Added CMakeLists + setup(clean/run/build) + some directories

---
+++ include/indie.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** indie-studio [WSL: Ubuntu]
+** File description:
+** indie
+*/
+
+#ifndef INDIE_HPP_
+#define INDIE_HPP_
+
+
+#endif /* !INDIE_HPP_ */
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** indie-studio [WSL: Ubuntu]
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const **argv)
+{
+    (void) argc;
+    (void) argv;
+    printf("Hello worlds;");
+    return 0;
+}
