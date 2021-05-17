5936fc598cf11783123511c97c451b25a2f51e21
0xArKrant <contact@arkrant.us>
2021-05-17 02:17:47 +0200

    [ADD] ✨  Shape/Rectangle .cpp/.hpp complete (raylib=

--- include/shape/Rectangle.cpp
+++ include/shape/Rectangle.cpp
@@ -18,3 +18,125 @@ void shape::Rectangle::DrawShape()
 {
    ::DrawRectangleV(this->_position, this->_size, this->_color);
 }
+
+shape::RectangleRec::RectangleRec(::Rectangle rec, ::Color color)
+{
+    this->_rec = rec;
+    this->_color = color;
+}
+
+void shape::RectangleRec::DrawShape()
+{
+    ::DrawRectangleRec(this->_rec, this->_color);
+}
+
+shape::RectanglePro::RectanglePro(::Rectangle rec, ::Vector2 origin, float rotation, ::Color color)
+{
+    this->_rec = rec;
+    this->_origin = origin;
+    this->_rotation = rotation;
+    this->_color = color;
+}
+
+void shape::RectanglePro::DrawShape()
+{
+    ::DrawRectanglePro(this->_rec, this->_origin, this->_rotation, this->_color);
+}
+
+shape::RectangleGradientV::RectangleGradientV(int posX, int posY, int width, int height, ::Color color, ::Color color2)
+{
+    this->_posX = posX;
+    this->_posY = posY;
+    this->_width = width;
+    this->_height = height;
+    this->_color = color;
+    this->_color2 = color2;
+}
+
+void shape::RectangleGradientV::DrawShape()
+{
+    ::DrawRectangleGradientV(this->_posX, this->_posY, this->_width, this->_height, this->_color, this->_color2);
+}
+
+shape::RectangleGradientH::RectangleGradientH(int posX, int posY, int width, int height, ::Color color, ::Color color2)
+{
+    this->_posX = posX;
+    this->_posY = posY;
+    this->_width = width;
+    this->_height = height;
+    this->_color = color;
+    this->_color2 = color2;
+}
+
+void shape::RectangleGradientH::DrawShape()
+{
+    ::DrawRectangleGradientH(this->_posX, this->_posY, this->_width, this->_height, this->_color, this->_color2);
+}
+
+shape::RectangleGradientEx::RectangleGradientEx(::Rectangle rec, ::Color color1, ::Color color2, ::Color color3, ::Color color4)
+{
+    this->_rec = rec;
+    this->_color1 = color1;
+    this->_color2 = color2;
+    this->_color3 = color3;
+    this->_color4 = color4;
+}
+
+void shape::RectangleGradientEx::DrawShape()
+{
+    ::DrawRectangleGradientEx(this->_rec, this->_color1, this->_color2, this->_color3, this->_color4);
+}
+
+shape::RectangleLines::RectangleLines(int posX, int posY, int width, int height, ::Color color)
+{
+    this->_posX = posX;
+    this->_posY = posY;
+    this->_width = width;
+    this->_height = height;
+    this->_color = color;
+}
+
+void shape::RectangleLines::DrawShape()
+{
+    ::DrawRectangleLines(this->_posX, this->_posY, this->_width, this->_height, this->_color);
+}
+
+shape::RectangleLinesEx::RectangleLinesEx(::Rectangle rec, int linethick, ::Color color)
+{
+    this->_rec = rec;
+    this->_linethick = linethick;
+    this->_color = color;
+}
+
+void shape::RectangleLinesEx::DrawShape()
+{
+    ::DrawRectangleLinesEx(this->_rec, this->_linethick, this->_color);
+}
+
+shape::RectangleRounded::RectangleRounded(::Rectangle rec, float roundness, int segments, ::Color color)
+{
+    this->_rec = rec;
+    this->_roundness = roundness;
+    this->_segments = segments;
+    this->_color = color;
+}
+
+void shape::RectangleRounded::DrawShape()
+{
+    ::DrawRectangleRounded(this->_rec, this->_roundness, this->_segments, this->_color);
+}
+
+shape::RectangleRoundedLines::RectangleRoundedLines(::Rectangle rec, float roundness, int segments, int linethick, ::Color color)
+{
+    this->_rec = rec;
+    this->_roundness = roundness;
+    this->_segments = segments;
+    this->_linethick = linethick;
+    this->_color = color;
+
+}
+
+void shape::RectangleRoundedLines::DrawShape()
+{
+    ::DrawRectangleRoundedLines(this->_rec, this->_roundness, this->_segments, this->_linethick, this->_color);
+}
--- include/shape/Rectangle.hpp
+++ include/shape/Rectangle.hpp
@@ -13,19 +13,138 @@
 namespace shape {
     class Rectangle : public Shape {
         public:
-            Rectangle(Vector2, Vector2, Color);
+            Rectangle(::Vector2, ::Vector2, ::Color);
             ~Rectangle() = default;
             void DrawShape();
-            Vector2 GetPosition() { return this->_position; };
-            void SetPosition(Vector2 position) { this->_position = position; };
-            Color GetColor() { return this->_color; };
+            ::Vector2 GetPosition() { return this->_position; };
+            void SetPosition(::Vector2 position) { this->_position = position; };
+            ::Color GetColor() { return this->_color; };
             void SetColor(Color color) { this->_color = color; };
         protected:
         private:
-            Vector2 _position;
-            Vector2 _size;
-            Color _color;
+            ::Vector2 _position;
+            ::Vector2 _size;
+            ::Color _color;
     };
+    class RectangleRec : public Shape {
+        public:
+            RectangleRec(::Rectangle, ::Color);
+            ~RectangleRec() = default;
+            void DrawShape();
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            ::Rectangle _rec;
+            ::Color _color;
+    };
+    class RectanglePro : public Shape {
+        public:
+            RectanglePro(::Rectangle, ::Vector2, float, ::Color);
+            ~RectanglePro() = default;
+            void DrawShape();
+            ::Vector2 GetPosition() { return this->_origin; };
+            void SetPosition(::Vector2 origin) { this->_origin = origin; };
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            ::Rectangle _rec;
+            ::Vector2 _origin;
+            float _rotation;
+            ::Color _color;
+    };
+    class RectangleGradientV : public Shape {
+        public:
+            RectangleGradientV(int, int, int, int, ::Color, ::Color);
+            ~RectangleGradientV() = default;
+            void DrawShape();
+        private:
+            int _posX;
+            int _posY;
+            int _width;
+            int _height;
+            ::Color _color;
+            ::Color _color2;
+    };
+    class RectangleGradientH : public Shape {
+        public:
+            RectangleGradientH(int, int, int, int, ::Color, ::Color);
+            ~RectangleGradientH() = default;
+            void DrawShape();
+        private:
+            int _posX;
+            int _posY;
+            int _width;
+            int _height;
+            ::Color _color;
+            ::Color _color2;
+    };
+    class RectangleGradientEx : public Shape {
+        public:
+            RectangleGradientEx(::Rectangle, ::Color, ::Color, ::Color, ::Color);
+            ~RectangleGradientEx() = default;
+            void DrawShape();
+        private:
+            ::Rectangle _rec;
+            ::Color _color1;
+            ::Color _color2;
+            ::Color _color3;
+            ::Color _color4;
+    };
+    class RectangleLines : public Shape {
+        public:
+            RectangleLines(int, int, int, int, ::Color);
+            ~RectangleLines() = default;
+            void DrawShape();
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            int _posX;
+            int _posY;
+            int _width;
+            int _height;
+            ::Color _color;
+    };
+    class RectangleLinesEx : public Shape {
+        public:
+            RectangleLinesEx(::Rectangle, int, ::Color);
+            ~RectangleLinesEx() = default;
+            void DrawShape();
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            ::Rectangle _rec;
+            int _linethick;
+            ::Color _color;
+    };
+    class RectangleRounded : public Shape {
+        public:
+            RectangleRounded(::Rectangle, float, int, ::Color);
+            ~RectangleRounded() = default;
+            void DrawShape();
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            ::Rectangle _rec;
+            float _roundness;
+            int _segments;
+            ::Color _color;
+    };
+    class RectangleRoundedLines : public Shape {
+        public:
+            RectangleRoundedLines(::Rectangle, float, int, int, ::Color);
+            ~RectangleRoundedLines() = default;
+            void DrawShape();
+            ::Color GetColor() { return this->_color; };
+            void SetColor(::Color color) { this->_color = color; };
+        private:
+            ::Rectangle _rec;
+            float _roundness;
+            int _segments;
+            int _linethick;
+            ::Color _color;
+
+    };
+
 }
 
 #endif /* !RECTANGLE_HPP_ */
