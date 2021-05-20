66055261626c54013aae898288ce29d17c61b54f
0xArKrant <contact@arkrant.us>
2021-05-20 18:52:49 +0200

    [REWORK] ⚡ Circle.cpp/.hpp Raylib -> Misc::Vector/Misc::Color

--- src/engine/Shape/Circle.cpp
+++ src/engine/Shape/Circle.cpp
@@ -7,84 +7,111 @@
 
 #include "indie.hpp"
 
-IShape::Circle::Circle(int centerX, int centerY, float radius, ::Color color)
+IShape::Circle::Circle(const int centerX, const int centerY, const float radius, Misc::Colors &values) : _colors(values)
 {
     this->_centerX = centerX;
     this->_centerY = centerY;
     this->_radius = radius;
-    this->_color = color;
 }
 
 void IShape::Circle::DrawShape()
 {
-    ::DrawCircle(this->_centerX, this->_centerY, this->_radius, this->_color);
+    ::DrawCircle(
+        this->_centerX, 
+        this->_centerY, 
+        this->_radius, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::CircleSector::CircleSector(::Vector2 center, float radius, float startAngle, float endAngle, int segments, ::Color color)
+IShape::CircleSector::CircleSector(const Misc::Vector<2> center, const float radius, const float startAngle, const float endAngle, const int segments, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_radius = radius;
     this->_startAngle = startAngle;
     this->_endAngle = endAngle;
     this->_segments = segments;
-    this->_color = color;
 }
 
 void IShape::CircleSector::DrawShape()
 {
-    ::DrawCircleSector(this->_center, this->_radius, this->_startAngle, this->_endAngle, this->_segments, this->_color);
+    ::DrawCircleSector(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_radius, 
+        this->_startAngle, 
+        this->_endAngle, 
+        this->_segments, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::CircleSectorLines::CircleSectorLines(::Vector2 center, float radius, float startAngle, float endAngle, int segments, ::Color color)
+IShape::CircleSectorLines::CircleSectorLines(const Misc::Vector<2> center, const float radius, const float startAngle, const float endAngle, const int segments, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_radius = radius;
     this->_startAngle = startAngle;
     this->_endAngle = endAngle;
     this->_segments = segments;
-    this->_color = color;
 }
 
 void IShape::CircleSectorLines::DrawShape()
 {
-    ::DrawCircleSectorLines(this->_center, this->_radius, this->_startAngle, this->_endAngle, this->_segments, this->_color);
+    ::DrawCircleSectorLines(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_radius, 
+        this->_startAngle, 
+        this->_endAngle, 
+        this->_segments, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::CircleGradient::CircleGradient(int centerX, int centerY, float radius, ::Color color1, ::Color color2)
+IShape::CircleGradient::CircleGradient(const int centerX, const int centerY, const float radius, Misc::Colors &values1, Misc::Colors &values2) : _colors1(values1), _colors2(values2)
 {
     this->_centerX = centerX;
     this->_centerY = centerY;
     this->_radius = radius;
-    this->_color1 = color1;
-    this->_color2 = color2;
 }
 
 void IShape::CircleGradient::DrawShape()
 {
-    ::DrawCircleGradient(this->_centerX, this->_centerY, this->_radius, this->_color1, this->_color2);
+    ::DrawCircleGradient(
+        this->_centerX, 
+        this->_centerY, 
+        this->_radius, 
+        ::Color { this->_colors1.getR(), this->_colors1.getG(), this->_colors1.getB(), this->_colors1.getA() },
+        ::Color { this->_colors2.getR(), this->_colors2.getG(), this->_colors2.getB(), this->_colors2.getA() }
+    );
 }
 
-IShape::CircleV::CircleV(::Vector2 center, float radius, ::Color color)
+IShape::CircleV::CircleV(const Misc::Vector<2> center, const float radius, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_radius = radius;
-    this->_color = color;
 }
 
 void IShape::CircleV::DrawShape()
 {
-    ::DrawCircleV(this->_center, this->_radius, this->_color);
+    ::DrawCircleV(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_radius, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::CircleLines::CircleLines(int centerX, int centerY, float radius, Color color)
+IShape::CircleLines::CircleLines(const int centerX, const int centerY, const float radius, Misc::Colors &values) : _colors(values)
 {
     this->_centerX = centerX;
     this->_centerY = centerY;
     this->_radius = radius;
-    this->_color = color;
 }
 
 void IShape::CircleLines::DrawShape()
 {
-    ::DrawCircleLines(this->_centerX, this->_centerY, this->_radius, this->_color);
+    ::DrawCircleLines(
+        this->_centerX, 
+        this->_centerY, 
+        this->_radius, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
--- src/engine/Shape/Circle.hpp
+++ src/engine/Shape/Circle.hpp
@@ -13,83 +13,152 @@
 namespace IShape {
     class Circle : public Shape {
         public:
-            Circle(int, int, float, ::Color);
+            Circle(const int, const int, const float, Misc::Colors &);
             ~Circle() = default;
+
+            inline int          GetCenterX() { return this->_centerX; };
+            inline int          GetCenterY() { return this->_centerY; };
+            inline float        GetRadius()  { return this->_radius;  };
+            inline Misc::Colors GetColors()  { return this->_colors;  };
+
+            inline void SetCenterX(int centerX)        { this->_centerX = centerX; };
+            inline void SetCenterY(int centerY)        { this->_centerY = centerY; };
+            inline void SetRadius(float radius)        { this->_radius = radius;   };
+            inline void SetColors(Misc::Colors colors) { this->_colors = colors;   };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            int _centerX;
-            int _centerY;
-            float _radius;
-            ::Color _color;
+            int          _centerX;
+            int          _centerY;
+            float        _radius;
+            Misc::Colors _colors;
     };
+
     class CircleSector : public Shape {
         public:
-            CircleSector(::Vector2, float, float, float, int, ::Color);
+            CircleSector(const Misc::Vector<2>, const float, const float, const float, const int, Misc::Colors &);
             ~CircleSector() = default;
+
+            inline Misc::Vector<2> GetVector()     { return this->_center;     };
+            inline float           GetRadius()     { return this->_radius;     };
+            inline float           GetStartAngle() { return this->_startAngle; };
+            inline float           GetEndAngle()   { return this->_endAngle;   };
+            inline int             GetSegments()   { return this->_segments;   };
+            inline Misc::Colors    GetColors()     { return this->_colors;     };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center;         };
+            inline void SetRadius(float radius)           { this->_radius = radius;         };
+            inline void SetStartAngle(float startAngle)   { this->_startAngle = startAngle; };
+            inline void SetEndAngle(float endAngle)       { this->_endAngle = endAngle;     };
+            inline void SetSegments(int segments)         { this->_segments = segments;     };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors;         };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _center;
-            float _radius;
-            float _startAngle;
-            float _endAngle;
-            int _segments;
-            ::Color _color;
+            Misc::Vector<2> _center;
+            float           _radius;
+            float           _startAngle;
+            float           _endAngle;
+            int             _segments;
+            Misc::Colors    _colors;
     };
+
     class CircleSectorLines : public Shape {
         public:
-            CircleSectorLines(::Vector2, float, float, float, int, ::Color);
+            CircleSectorLines(const Misc::Vector<2>, const float, const float, const float, const int, Misc::Colors &);
             ~CircleSectorLines() = default;
+
+            inline Misc::Vector<2> GetVector()     { return this->_center;     };
+            inline float           GetRadius()     { return this->_radius;     };
+            inline float           GetStartAngle() { return this->_startAngle; };
+            inline float           GetEndAngle()   { return this->_endAngle;   };
+            inline int             GetSegments()   { return this->_segments;   };
+            inline Misc::Colors    GetColors()     { return this->_colors;     };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center;         };
+            inline void SetRadius(float radius)           { this->_radius = radius;         };
+            inline void SetStartAngle(float startAngle)   { this->_startAngle = startAngle; };
+            inline void SetEndAngle(float endAngle)       { this->_endAngle = endAngle;     };
+            inline void SetSegments(int segments)         { this->_segments = segments;     };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors;         };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _center;
-            float _radius;
-            float _startAngle;
-            float _endAngle;
-            int _segments;
-            ::Color _color;
+            Misc::Vector<2> _center;
+            float           _radius;
+            float           _startAngle;
+            float           _endAngle;
+            int             _segments;
+            Misc::Colors    _colors;
     };
+
     class CircleGradient : public Shape {
         public:
-            CircleGradient(int, int, float, ::Color, ::Color);
+            CircleGradient(const int, const int, const float, Misc::Colors &, Misc::Colors &);
             ~CircleGradient() = default;
+
+            inline int          GetCenterX() { return this->_centerX; };
+            inline int          GetCenterY() { return this->_centerY; };
+            inline float        GetRadius()  { return this->_radius;  }
+            inline Misc::Colors GetColors1() { return this->_colors1; };
+            inline Misc::Colors GetColors2() { return this->_colors2; };
+
+            inline void SetCenterX(int centerX)          { this->_centerX = centerX; };
+            inline void SetCenterY(int centerY)          { this->_centerY = centerY; };
+            inline void SetRadius(float radius)          { this->_radius = radius;   };
+            inline void SetColors1(Misc::Colors colors1) { this->_colors1 = colors1; };
+            inline void SetColors2(Misc::Colors colors2) { this->_colors2 = colors2; };
+
             void DrawShape();
         private:
-            int _centerX;
-            int _centerY;
-            float _radius;
-            ::Color _color1;
-            ::Color _color2;
+            int          _centerX;
+            int          _centerY;
+            float        _radius;
+            Misc::Colors _colors1;
+            Misc::Colors _colors2;
     };
+
     class CircleV : public Shape {
         public:
-            CircleV(::Vector2, float, ::Color);
+            CircleV(const Misc::Vector<2>, const float, Misc::Colors &);
             ~CircleV() = default;
+
+            inline Misc::Vector<2> GetVector() { return this->_center; };
+            inline float           GetRadius() { return this->_radius; }
+            inline Misc::Colors    GetColors() { return this->_colors; };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center; };
+            inline void SetRadius(float radius)           { this->_radius = radius; };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors; };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _center;
-            float _radius;
-            ::Color _color;
+            Misc::Vector<2>  _center;
+            float            _radius;
+            Misc::Colors     _colors;
     };
+
     class CircleLines : public Shape {
         public:
-            CircleLines(int, int, float, ::Color);
+            CircleLines(const int, const int, const float, Misc::Colors &);
             ~CircleLines() = default;
+
+            inline int          GetCenterX() { return this->_centerX; };
+            inline int          GetCenterY() { return this->_centerY; };
+            inline float        GetRadius()  { return this->_radius;  };
+            inline Misc::Colors GetColors()  { return this->_colors;  };
+
+            inline void SetCenterX(int centerX)        { this->_centerX = centerX; };
+            inline void SetCenterY(int centerY)        { this->_centerY = centerY; };
+            inline void SetRadius(float radius)        { this->_radius = radius;   };
+            inline void SetColors(Misc::Colors colors) { this->_colors = colors;   };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            int _centerX;
-            int _centerY;
-            float _radius;
-            ::Color _color;
+            int          _centerX;
+            int          _centerY;
+            float        _radius;
+            Misc::Colors _colors;
     };
 }
 
