1c545a46beaf94ef0fd70dae62d55d583c78d31b
0xArKrant <contact@arkrant.us>
2021-05-20 19:27:02 +0200

    [REWORK] ⚡ Line.cpp/.hpp Raylib -> Misc::Vector/Misc::Color

--- src/engine/Shape/Line.cpp
+++ src/engine/Shape/Line.cpp
@@ -7,81 +7,107 @@
 
 #include "indie.hpp"
 
-IShape::Line::Line(int startPosX, int startPosY, int endPosX, int endPosY, ::Color color)
+IShape::Line::Line( const int startPosX, const int startPosY, const int endPosX, const int endPosY, Misc::Colors &values) : _colors(values)
 {
     this->_startPosX = startPosX;
     this->_startPosY = startPosY;
     this->_endPosX = endPosX;
     this->_endPosY = endPosY;
-    this->_color = color;
 }
 
 void IShape::Line::DrawShape()
 {
-    ::DrawLine(this->_startPosX, this->_startPosY, this->_endPosX, this->_endPosY, this->_color);
+    ::DrawLine(
+        this->_startPosX, 
+        this->_startPosY, 
+        this->_endPosX, 
+        this->_endPosY, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::LineV::LineV(::Vector2 startPos, ::Vector2 endPos, ::Color color)
+IShape::LineV::LineV(const Misc::Vector<2> startPos, const Misc::Vector<2> endPos, Misc::Colors &values) : _colors(values)
 {
     this->_startPos = startPos;
     this->_endPos = endPos;
-    this->_color = color;
 }
 
 void IShape::LineV::DrawShape()
 {
-    ::DrawLineV(this->_startPos, this->_endPos, this->_color);
+    ::DrawLineV(
+        ::Vector2 { this->_startPos.getX(), this->_startPos.getY() },
+        ::Vector2 { this->_endPos.getX(), this->_endPos.getY() },
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::LineEx::LineEx(::Vector2 startPos, ::Vector2 endPos, float thick, ::Color color)
+IShape::LineEx::LineEx(const Misc::Vector<2> startPos, const Misc::Vector<2> endPos, const float thick, Misc::Colors &values) : _colors(values)
 {
     this->_startPos = startPos;
     this->_endPos = endPos;
     this->_thick = thick;
-    this->_color = color;
 }
 
 void IShape::LineEx::DrawShape()
 {
-    ::DrawLineEx(this->_startPos, this->_endPos, this->_thick, this->_color);
+    ::DrawLineEx(
+        ::Vector2 { this->_startPos.getX(), this->_startPos.getY() },
+        ::Vector2 { this->_endPos.getX(), this->_endPos.getY() },
+        this->_thick,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::LineBezier::LineBezier(::Vector2 startPos, ::Vector2 endPos, float thick, ::Color color)
+IShape::LineBezier::LineBezier(const Misc::Vector<2> startPos, const Misc::Vector<2> endPos, const float thick, Misc::Colors &values) : _colors(values)
 {
     this->_startPos = startPos;
     this->_endPos = endPos;
     this->_thick = thick;
-    this->_color = color;
 }
 
 void IShape::LineBezier::DrawShape()
 {
-    ::DrawLineBezier(this->_startPos, this->_endPos, this->_thick, this->_color);
+    ::DrawLineBezier(
+        ::Vector2 { this->_startPos.getX(), this->_startPos.getY() },
+        ::Vector2 { this->_endPos.getX(), this->_endPos.getY() },
+        this->_thick,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
 
-IShape::LineBezierQuad::LineBezierQuad(::Vector2 startPos, ::Vector2 endPos, ::Vector2 controlPos, float thick, ::Color color)
+IShape::LineBezierQuad::LineBezierQuad(const Misc::Vector<2> startPos, const Misc::Vector<2> endPos, const Misc::Vector<2> controlPos, const float thick, Misc::Colors &values) : _colors(values)
 {
     this->_startPos = startPos;
     this->_endPos = endPos;
     this->_controlPos = controlPos;
     this->_thick = thick;
-    this->_color = color;
 }
 
 void IShape::LineBezierQuad::DrawShape()
 {
-    ::DrawLineBezierQuad(this->_startPos, this->_endPos, this->_controlPos, this->_thick, this->_color);
+    ::DrawLineBezierQuad(
+        ::Vector2 { this->_startPos.getX(), this->_startPos.getY() },
+        ::Vector2 { this->_endPos.getX(), this->_endPos.getY() },
+        ::Vector2 { this->_controlPos.getX(), this->_controlPos.getY() },
+        this->_thick,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::LineStrip::LineStrip(::Vector2 *points, int pointsCount, ::Color color)
+IShape::LineStrip::LineStrip(const Misc::Vector<2> points, const int pointsCount, Misc::Colors &values) : _colors(values)
 {
     this->_points = points;
     this->_pointsCount = pointsCount;
-    this->_color = color;
 }
 
 void IShape::LineStrip::DrawShape()
 {
-    ::DrawLineStrip(this->_points, this->_pointsCount, this->_color);
+    ::Vector2 points = { this->_points.getX(), this->_points.getY() };
+
+    ::DrawLineStrip(
+        &points, 
+        this->_pointsCount, 
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
--- src/engine/Shape/Line.hpp
+++ src/engine/Shape/Line.hpp
@@ -13,81 +13,140 @@
 namespace IShape {
     class Line : public Shape {
         public:
-            Line(int, int, int, int, ::Color);
+            Line(const int, const int, const int, const int, Misc::Colors &);
             ~Line() = default;
+
+            inline int             GetStartPosX() { return this->_startPosX; };
+            inline int             GetStartPosY() { return this->_startPosY; };
+            inline int             GetEndPosX()   { return this->_endPosX;   };
+            inline int             GetEndPosY()   { return this->_endPosY;   };
+            inline Misc::Colors    GetColors()    { return this->_colors;    };
+
+            inline void SetStartPosX(int startPosX)    { this->_startPosX = startPosX; };
+            inline void SetStartPosY(int startPosY)    { this->_startPosY = startPosY; };
+            inline void SetEndPosX(int endPosX)        { this->_endPosX = endPosX;     };
+            inline void SetEndPosY(int endPosY)        { this->_endPosY = endPosY;     };
+            inline void SetColors(Misc::Colors colors) { this->_colors = colors;       };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            int _startPosX;
-            int _startPosY;
-            int _endPosX;
-            int _endPosY;
-            ::Color _color;
+            int          _startPosX;
+            int          _startPosY;
+            int          _endPosX;
+            int          _endPosY;
+            Misc::Colors _colors;
     };
+
     class LineV : public Shape {
         public:
-            LineV(::Vector2, ::Vector2, ::Color);
+            LineV(const Misc::Vector<2>, const Misc::Vector<2>, Misc::Colors &);
             ~LineV() = default;
+
+            inline Misc::Vector<2> GetVectorStartPos() { return this->_startPos; };
+            inline Misc::Vector<2> GetVectorEndPos()   { return this->_endPos;   };
+            inline Misc::Colors    GetColors()         { return this->_colors;   };
+
+            inline void SetVectorStartPosX(Misc::Vector<2> startPosX) { this->_startPos = startPosX; };
+            inline void SetVectorEndPosX(Misc::Vector<2> endPosX)     { this->_endPos = endPosX;     };
+            inline void SetColors(Misc::Colors colors)                { this->_colors = colors;      };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _startPos;
-            ::Vector2 _endPos;
-            ::Color _color;
+            Misc::Vector<2> _startPos;
+            Misc::Vector<2> _endPos;
+            Misc::Colors    _colors;
     };
+
     class LineEx : public Shape {
         public:
-            LineEx(::Vector2, ::Vector2, float, ::Color);
+            LineEx(const Misc::Vector<2>, const Misc::Vector<2>, const float, Misc::Colors &);
             ~LineEx() = default;
+
+            inline Misc::Vector<2> GetVectorStartPos() { return this->_startPos; };
+            inline Misc::Vector<2> GetVectorEndPos()   { return this->_endPos;   };
+            inline float           GetThickness()      { return this->_thick;    };
+            inline Misc::Colors    GetColors()         { return this->_colors;   };
+
+            inline void SetVectorStartPosX(Misc::Vector<2> startPosX) { this->_startPos = startPosX; };
+            inline void SetVectorEndPosX(Misc::Vector<2> endPosX)     { this->_endPos = endPosX;     };
+            inline void SetThickness(float thickness)                 { this->_thick =  thickness;   }; 
+            inline void SetColors(Misc::Colors colors)                { this->_colors = colors;      };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _startPos;
-            ::Vector2 _endPos;
-            float _thick;
-            ::Color _color;
+            Misc::Vector<2> _startPos;
+            Misc::Vector<2> _endPos;
+            float           _thick;
+            Misc::Colors    _colors;
     };
+
     class LineBezier : public Shape {
         public:
-            LineBezier(::Vector2, ::Vector2, float, ::Color);
+            LineBezier(const Misc::Vector<2>, const Misc::Vector<2>, const float, Misc::Colors &);
             ~LineBezier() = default;
+
+            inline Misc::Vector<2> GetVectorStartPos() { return this->_startPos; };
+            inline Misc::Vector<2> GetVectorEndPos()   { return this->_endPos;   };
+            inline float           GetThickness()      { return this->_thick;    };
+            inline Misc::Colors    GetColors()         { return this->_colors;   };
+
+            inline void SetVectorStartPosX(Misc::Vector<2> startPosX) { this->_startPos = startPosX; };
+            inline void SetVectorEndPosX(Misc::Vector<2> endPosX)     { this->_endPos = endPosX;     };
+            inline void SetThickness(float thickness)                 { this->_thick =  thickness;   }; 
+            inline void SetColors(Misc::Colors colors)                { this->_colors = colors;      };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _startPos;
-            ::Vector2 _endPos;
-            float _thick;
-            ::Color _color;
+            Misc::Vector<2> _startPos;
+            Misc::Vector<2> _endPos;
+            float           _thick;
+            Misc::Colors    _colors;
     };
+
     class LineBezierQuad : public Shape {
         public:
-            LineBezierQuad(::Vector2, ::Vector2, ::Vector2, float, ::Color);
+            LineBezierQuad(const Misc::Vector<2>, const Misc::Vector<2>, const Misc::Vector<2>, const float, Misc::Colors &);
             ~LineBezierQuad() = default;
+
+            inline Misc::Vector<2> GetVectorStartPos()   { return this->_startPos;   };
+            inline Misc::Vector<2> GetVectorEndPos()     { return this->_endPos;     };
+            inline Misc::Vector<2> GetVectorControlPos() { return this->_controlPos; };
+            inline float           GetThickness()        { return this->_thick;      };
+            inline Misc::Colors    GetColors()           { return this->_colors;     };
+
+            inline void SetVectorStartPosX(Misc::Vector<2> startPosX)   { this->_startPos = startPosX;    };
+            inline void SetVectorEndPosX(Misc::Vector<2> endPosX)       { this->_endPos = endPosX;        };
+            inline void SetVectorControlPos(Misc::Vector<2> controlPos) { this->_controlPos = controlPos; };
+            inline void SetThickness(float thickness)                   { this->_thick =  thickness;      }; 
+            inline void SetColors(Misc::Colors colors)                  { this->_colors = colors;         };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 _startPos;
-            ::Vector2 _endPos;
-            ::Vector2 _controlPos;
-            float _thick;
-            ::Color _color;
+            Misc::Vector<2> _startPos;
+            Misc::Vector<2> _endPos;
+            Misc::Vector<2> _controlPos;
+            float           _thick;
+            Misc::Colors    _colors;
     };
     class LineStrip : public Shape {
         public:
-            LineStrip(::Vector2 *, int, ::Color);
+            LineStrip(Misc::Vector<2>, int, Misc::Colors &);
             ~LineStrip() = default;
+
+            inline Misc::Vector<2> GetVectorPoints() { return this->_points;      };
+            inline int             GetPointsCount()  { return this->_pointsCount; };
+            inline Misc::Colors    GetColors()       { return this->_colors;      };
+
+            inline void SetVectorPoints(Misc::Vector<2> points) { this->_points = points;           };
+            inline void SetPointsCount(int pointsCount)         { this->_pointsCount = pointsCount; };
+            inline void SetColors(Misc::Colors colors)          { this->_colors = colors;           };
+
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector2 *_points;
+            Misc::Vector<2> _points;
             int _pointsCount;
-            ::Color _color;
+            Misc::Colors _colors;
     };
 }
 
