de1105c2246c6cf87573b9e93b90c8e786977932
0xArKrant <contact@arkrant.us>
2021-05-20 17:40:46 +0200

    [REWORK] ⚡ Polygon.cpp/.hpp Raylib -> Misc::Vector/Misc::Color

--- src/engine/Shape/Polygon.cpp
+++ src/engine/Shape/Polygon.cpp
@@ -7,48 +7,64 @@
 
 #include "indie.hpp"
 
-IShape::Polygon::Polygon(::Vector2 center, int sides, float radius, float rotation, ::Color color)
+IShape::Polygon::Polygon(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_sides = sides;
     this->_radius = radius;
     this->_rotation = rotation;
-    this->_color = color;
 }
 
 void IShape::Polygon::DrawShape()
 {
-    ::DrawPoly(this->_center, this->_sides, this->_radius, this->_rotation, this->_color);
+    ::DrawPoly(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_sides,
+        this->_radius,
+        this->_rotation,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
 
-IShape::PolygonLines::PolygonLines(::Vector2 center, int sides, float radius, float rotation, ::Color color)
+IShape::PolygonLines::PolygonLines(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_sides = sides;
     this->_radius = radius;
     this->_rotation = rotation;
-    this->_color = color;
 }
 
 void IShape::PolygonLines::DrawShape()
 {
-    ::DrawPolyLines(this->_center, this->_sides, this->_radius, this->_rotation, this->_color);
+    ::DrawPolyLines(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_sides,
+        this->_radius,
+        this->_rotation,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
-IShape::PolygonLinesEx::PolygonLinesEx(::Vector2 center, int sides, float radius, float rotation, float linethick, ::Color color)
+IShape::PolygonLinesEx::PolygonLinesEx(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, const float linethick, Misc::Colors &values) : _colors(values)
 {
     this->_center = center;
     this->_sides = sides;
     this->_radius = radius;
     this->_rotation = rotation;
     this->_linethick = linethick;
-    this->_color = color;
 }
 
 void IShape::PolygonLinesEx::DrawShape()
 {
-    ::DrawPolyLinesEx(this->_center, this->_sides, this->_radius, this->_rotation, this->_linethick, this->_color);
+    ::DrawPolyLinesEx(
+        ::Vector2 { this->_center.getX(), this->_center.getY() },
+        this->_sides,
+        this->_radius,
+        this->_rotation,
+        this->_linethick,
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
 
--- src/engine/Shape/Polygon.hpp
+++ src/engine/Shape/Polygon.hpp
@@ -13,46 +13,83 @@
 namespace IShape {
     class Polygon : public Shape {
         public:
-            Polygon(::Vector2, int, float, float, ::Color);
+            Polygon(const Misc::Vector<2>, const int, const float, const float, Misc::Colors &);
             ~Polygon() = default;
+
+            inline Misc::Vector<2> GetVector()   { return this->_center;      };
+            inline int             GetSides()    { return this->_sides;       };
+            inline float           GetRadius()   { return this->_radius;      };
+            inline float           GetRotation() { return this->_rotation;    };
+            inline Misc::Colors    GetColors()   { return this->_colors;      };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center;           };
+            inline void SetSides(int sides)               { this->_sides = sides;             };
+            inline void SetRadius(float radius)           { this->_radius = radius;           };
+            inline void SetRotation(float rotation)       { this->_rotation = rotation;       };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors;           };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            Vector2 _center;
-            int _sides;
-            float _radius;
-            float _rotation;
-            ::Color _color;
+            Misc::Vector<2> _center;
+            int             _sides;
+            float           _radius;
+            float           _rotation;
+            Misc::Colors    _colors;
     };
+
     class PolygonLines : public Shape {
         public:
-            PolygonLines(::Vector2, int, float, float, ::Color);
+            PolygonLines(const Misc::Vector<2>, const int, const float, const float, Misc::Colors &);
             ~PolygonLines() = default;
+
+            inline Misc::Vector<2> GetVector()   { return this->_center;      };
+            inline int             GetSides()    { return this->_sides;       };
+            inline float           GetRadius()   { return this->_radius;      };
+            inline float           GetRotation() { return this->_rotation;    };
+            inline Misc::Colors    GetColors()   { return this->_colors;      };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center;           };
+            inline void SetSides(int sides)               { this->_sides = sides;             };
+            inline void SetRadius(float radius)           { this->_radius = radius;           };
+            inline void SetRotation(float rotation)       { this->_rotation = rotation;       };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors;           };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            Vector2 _center;
-            int _sides;
-            float _radius;
-            float _rotation;
-            ::Color _color;
+            Misc::Vector<2> _center;
+            int             _sides;
+            float           _radius;
+            float           _rotation;
+            Misc::Colors    _colors;
     };
+
     class PolygonLinesEx : public Shape {
         public:
-            PolygonLinesEx(::Vector2, int, float, float, float, ::Color);
+            PolygonLinesEx(const Misc::Vector<2>, const int, const float, const float, const float, Misc::Colors &);
             ~PolygonLinesEx() = default;
+
+            inline Misc::Vector<2> GetVector()    { return this->_center;      };
+            inline int             GetSides()     { return this->_sides;       };
+            inline float           GetRadius()    { return this->_radius;      };
+            inline float           GetRotation()  { return this->_rotation;    };
+            inline float           GetLineThick() { return this->_linethick;   };
+            inline Misc::Colors    GetColors()    { return this->_colors;      };
+
+            inline void SetVector(Misc::Vector<2> center) { this->_center = center;           };
+            inline void SetSides(int sides)               { this->_sides = sides;             };
+            inline void SetRadius(float radius)           { this->_radius = radius;           };
+            inline void SetRotation(float rotation)       { this->_rotation = rotation;       };
+            inline void SetLineThick(float linethick)     { this->_linethick = linethick;     };
+            inline void SetColors(Misc::Colors colors)    { this->_colors = colors;           };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            Vector2 _center;
-            int _sides;
-            float _radius;
-            float _rotation;
-            float _linethick;
-            ::Color _color;
+            Misc::Vector<2> _center;
+            int             _sides;
+            float           _radius;
+            float           _rotation;
+            float           _linethick;
+            Misc::Colors    _colors;
     };
 }
 
