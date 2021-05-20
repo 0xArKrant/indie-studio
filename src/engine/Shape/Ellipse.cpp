8308027b9b3e74d4792abc3cbabbd9488a9f88e3
0xArKrant <contact@arkrant.us>
2021-05-20 17:28:15 +0200

    [REWORK] ⚡ Ellipse.cpp/.hpp Raylib -> Misc::Vector/Misc::Color

--- src/engine/Shape/Ellipse.cpp
+++ src/engine/Shape/Ellipse.cpp
@@ -7,31 +7,41 @@
 
 #include "indie.hpp"
 
-IShape::Ellipse::Ellipse(int centerX, int centerY, float radiusH, float radiusV, ::Color color)
+IShape::Ellipse::Ellipse(const int centerX, const int centerY, const float radiusH, const float radiusV, Misc::Colors &values) : _colors(values)
 {
     this->_centerX = centerX;
     this->_centerY = centerY;
     this->_radiusH = radiusH;
     this->_radiusV = radiusV;
-    this->_color = color;
 }
 
 void IShape::Ellipse::DrawShape()
 {
-    ::DrawEllipse(this->_centerX, this->_centerY, this->_radiusH, this->_radiusV, this->_color);
+    ::DrawEllipse(
+        this->_centerX,
+        this->_centerY,
+        this->_radiusH,
+        this->_radiusV,
+        Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
 
 
-IShape::EllipseLines::EllipseLines(int centerX, int centerY, float radiusH, float radiusV, ::Color color)
+IShape::EllipseLines::EllipseLines(const int centerX, const int centerY, const float radiusH, const float radiusV, Misc::Colors &values) : _colors(values)
 {
     this->_centerX = centerX;
     this->_centerY = centerY;
     this->_radiusH = radiusH;
     this->_radiusV = radiusV;
-    this->_color = color;
 }
 
 void IShape::EllipseLines::DrawShape()
 {
-    ::DrawEllipseLines(this->_centerX, this->_centerY, this->_radiusH, this->_radiusV, this->_color);
+    ::DrawEllipseLines(
+        this->_centerX,
+        this->_centerY,
+        this->_radiusH,
+        this->_radiusV,
+        Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
--- src/engine/Shape/Ellipse.hpp
+++ src/engine/Shape/Ellipse.hpp
@@ -13,31 +13,54 @@
 namespace IShape {
     class Ellipse : public Shape {
         public:
-            Ellipse(int, int, float, float, ::Color);
+            Ellipse(const int, const int, const float, const float, Misc::Colors &);
             ~Ellipse() = default;
+
+            inline int          GetCenterX() { return this->_centerX; };
+            inline int          GetCenterY() { return this->_centerY; };
+            inline float        GetRadiusH() { return this->_radiusH; };
+            inline float        GetRadiusV() { return this->_radiusV; };
+            inline Misc::Colors GetColors()  { return this->_colors;  };
+
+            inline void SetCenterX(int centerX)        { this->_centerX = centerX; };
+            inline void SetCenterY(int centerY)        { this->_centerY = centerY; };
+            inline void SetRadiusH(float radiusH)      { this->_radiusH = radiusH; };
+            inline void SetRadiusV(float radiusV)      { this->_radiusV = radiusV; };
+            inline void SetColors(Misc::Colors colors) { this->_colors = colors;   };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            int _centerX;
-            int _centerY;
-            float _radiusH;
-            float _radiusV;
-            ::Color _color;
+            int          _centerX;
+            int          _centerY;
+            float        _radiusH;
+            float        _radiusV;
+            Misc::Colors _colors;
     };
+
     class EllipseLines : public Shape {
         public:
-            EllipseLines(int, int, float, float, ::Color);
+            EllipseLines(const int, const int, const float, const float, Misc::Colors &);
             ~EllipseLines() = default;
+
+            inline int          GetCenterX() { return this->_centerX; };
+            inline int          GetCenterY() { return this->_centerY; };
+            inline float        GetRadiusH() { return this->_radiusH; };
+            inline float        GetRadiusV() { return this->_radiusV; };
+            inline Misc::Colors GetColors()  { return this->_colors;  };
+
+            inline void SetCenterX(int centerX)        { this->_centerX = centerX; };
+            inline void SetCenterY(int centerY)        { this->_centerY = centerY; };
+            inline void SetRadiusH(float radiusH)      { this->_radiusH = radiusH; };
+            inline void SetRadiusV(float radiusV)      { this->_radiusV = radiusV; };
+            inline void SetColors(Misc::Colors colors) { this->_colors = colors;   };
+
             void DrawShape();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            int _centerX;
-            int _centerY;
-            float _radiusH;
-            float _radiusV;
-            ::Color _color;
+            int          _centerX;
+            int          _centerY;
+            float        _radiusH;
+            float        _radiusV;
+            Misc::Colors _colors;
     };
 }
 
