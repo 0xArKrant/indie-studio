438422c9c0602d4fea8ac1ccb7c543ad1ea3ca22
0xArKrant <contact@arkrant.us>
2021-05-24 19:10:52 +0200

    [REWORK] ⚡ Complete rework of Line3D.cpp/.hpp Raylib

--- src/engine/Models/Geometric/Line3D.cpp
+++ src/engine/Models/Geometric/Line3D.cpp
@@ -7,14 +7,18 @@
 
 #include "indie.hpp"
 
-IModels::Line3D::Line3D(::Vector3 startPos, ::Vector3 endPos, ::Color color)
+IModels::Line3D::Line3D(const Misc::Vector<3> startPos, const Misc::Vector<3> endPos, const Misc::Colors &values)
 {
     this->_startPos = startPos;
     this->_endPos = endPos;
-    this->_color = color;
+    this->_colors = values;
 }
 
 void IModels::Line3D::DrawModels()
 {
-    ::DrawLine3D(this->_startPos, this->_endPos, this->_color);
+    ::DrawLine3D(
+        ::Vector3 { this->_startPos.getX(), this->_startPos.getY(), this->_startPos.getZ() },
+        ::Vector3 { this->_endPos.getX(), this->_endPos.getY(), this->_endPos.getZ() },
+        ::Color { this->_colors.getR(), this->_colors.getG(), this->_colors.getB(), this->_colors.getA() }
+    );
 }
--- src/engine/Models/Geometric/Line3D.hpp
+++ src/engine/Models/Geometric/Line3D.hpp
@@ -13,15 +13,22 @@
 namespace IModels {
     class Line3D : public Models {
         public:
-            Line3D(::Vector3, ::Vector3, ::Color);
+            Line3D(const Misc::Vector<3>, const Misc::Vector<3>, const Misc::Colors &);
             ~Line3D() = default;
+
+            inline Misc::Vector<3> GetStartPos() const { return this->_startPos; };
+            inline Misc::Vector<3> GetEndPos()   const { return this->_endPos;   };
+            inline Misc::Colors    GetColors()   const { return this->_colors;   };
+
+            inline void SetStartPos(const Misc::Vector<3> startPos) { this->_startPos = startPos; };
+            inline void SetEndPos(const Misc::Vector<3> endPos)     { this->_endPos = endPos;     };
+            inline void SetColors(const Misc::Colors colors)        { this->_colors = colors;     };
+
             void DrawModels();
-            ::Color GetColor() { return this->_color; };
-            void SetColor(::Color color) { this->_color = color; };
         private:
-            ::Vector3 _startPos;
-            ::Vector3 _endPos;
-            ::Color _color;
+            Misc::Vector<3> _startPos;
+            Misc::Vector<3> _endPos;
+            Misc::Colors    _colors;
     };
 }
 
