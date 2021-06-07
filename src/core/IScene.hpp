fbb5508155e5a08f18b715f520811921f92427ac
Azenki <azncharles@zone-dxd.fr>
2021-06-07 12:35:25 +0200

    [UPDATE] indie.hpp and add namespace

--- src/core/IScene.hpp
+++ src/core/IScene.hpp
@@ -10,15 +10,18 @@
 
 #include "indie.hpp"
 
-class IScene {
-    public:
-        ~IScene() = default;
-        virtual void update() = 0;
-        virtual void  draw() = 0;
-        virtual std::string getName() = 0;
+namespace Core
+{
+    class IScene {
+        public:
+            ~IScene() = default;
+            virtual void update() = 0;
+            virtual void  draw() = 0;
+            virtual std::string getName() = 0;
 
-    protected:
-    private:
-};
+        protected:
+        private:
+    };
+}
 
 #endif /* !ISCENE_HPP_ */
