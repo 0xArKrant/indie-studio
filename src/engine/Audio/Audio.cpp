1e2db5530c1f84d82b07b2c3e9204c3205dcb761
0xArKrant <contact@arkrant.us>
2021-06-09 19:17:19 +0200

    [UPDATED] ✨ Namespace Raylib for Audio/

--- src/engine/Audio/Audio.cpp
+++ src/engine/Audio/Audio.cpp
@@ -7,52 +7,52 @@
 
 #include "Audio.hpp"
 
-Audio::Wave::Wave(std::string fileName)
+Raylib::Audio::Wave::Wave(std::string fileName)
 {
     this->_wave = ::LoadWave(fileName.c_str());
 }
 
-Audio::Wave::Wave(std::string fileName, const unsigned char *fileData, int dataSize)
+Raylib::Audio::Wave::Wave(std::string fileName, const unsigned char *fileData, int dataSize)
 {
     this->_wave = ::LoadWaveFromMemory(fileName.c_str(), fileData, dataSize);
 }
 
-Audio::Wave::~Wave()
+Raylib::Audio::Wave::~Wave()
 {
     ::UnloadWave(this->_wave);
 }
 
-Audio::Sound::Sound(std::string fileName)
+Raylib::Audio::Sound::Sound(std::string fileName)
 {
     this->_sound = ::LoadSound(fileName.c_str());
 }
 
-Audio::Sound::~Sound()
+Raylib::Audio::Sound::~Sound()
 {
     ::UnloadSound(this->_sound);
 }
 
-Audio::Music::Music(std::string fileName)
+Raylib::Audio::Music::Music(std::string fileName)
 {
     this->_music = ::LoadMusicStream(fileName.c_str());
 }
 
-Audio::Music::Music(std::string fileType, unsigned char *data, int dataSize)
+Raylib::Audio::Music::Music(std::string fileType, unsigned char *data, int dataSize)
 {
     this->_music = ::LoadMusicStreamFromMemory(fileType.c_str(), data, dataSize);
 }
 
-Audio::Music::~Music()
+Raylib::Audio::Music::~Music()
 {
     ::UnloadMusicStream(this->_music);
 }
 
-Audio::AudioStream::AudioStream(unsigned int sampleRate, unsigned int sampleSize, unsigned int channels)
+Raylib::Audio::AudioStream::AudioStream(unsigned int sampleRate, unsigned int sampleSize, unsigned int channels)
 {
     this->_audiostream = ::LoadAudioStream(sampleRate, sampleSize, channels);
 }
 
-Audio::AudioStream::~AudioStream()
+Raylib::Audio::AudioStream::~AudioStream()
 {
     ::UnloadAudioStream(this->_audiostream);
 }
--- src/engine/Audio/Audio.hpp
+++ src/engine/Audio/Audio.hpp
@@ -8,21 +8,26 @@
 #pragma once
 
 #include "indie.hpp"
-
-namespace Audio {
-    class Wave {
+namespace Raylib
+{
+    namespace Audio
+    {
+        class Wave
+        {
         public:
             Wave(std::string);
             Wave(std::string, const unsigned char *, int);
             ~Wave();
 
-            inline bool ExportWave(std::string fileName)       const { return ::ExportWave(this->_wave, fileName.c_str());       };
+            inline bool ExportWave(std::string fileName) const { return ::ExportWave(this->_wave, fileName.c_str()); };
             inline bool ExportWaveAsCode(std::string fileName) const { return ::ExportWaveAsCode(this->_wave, fileName.c_str()); };
+
         private:
             ::Wave _wave;
-    };
+        };
 
-    class Sound {
+        class Sound
+        {
         public:
             Sound(std::string);
             ~Sound();
@@ -31,45 +36,49 @@ namespace Audio {
 
             inline bool isSoundPlaying(void) const { return ::IsSoundPlaying(this->_sound); };
 
-            inline void PlaySound(void)      const { ::PlaySound(this->_sound);      };
-            inline void StopSound(void)      const { ::StopSound(this->_sound);      };
-            inline void PauseSound(void)     const { ::PauseSound(this->_sound);     };
-            inline void ResumeSound(void)    const { ::ResumeSound(this->_sound);    };
+            inline void PlaySound(void) const { ::PlaySound(this->_sound); };
+            inline void StopSound(void) const { ::StopSound(this->_sound); };
+            inline void PauseSound(void) const { ::PauseSound(this->_sound); };
+            inline void ResumeSound(void) const { ::ResumeSound(this->_sound); };
             inline void PlaySoundMulti(void) const { ::PlaySoundMulti(this->_sound); };
-            inline void StopSoundMulti(void) const { ::StopSoundMulti();             };
+            inline void StopSoundMulti(void) const { ::StopSoundMulti(); };
 
             inline int GetSoundsPlaying(void) const { return ::GetSoundsPlaying(); };
 
             inline void SetSoundVolume(float volume) const { ::SetSoundVolume(this->_sound, volume); };
-            inline void SetSoundPitch(float pitch)   const { ::SetSoundPitch(this->_sound, pitch);   };
+            inline void SetSoundPitch(float pitch) const { ::SetSoundPitch(this->_sound, pitch); };
+
         private:
             ::Sound _sound;
-    };
+        };
 
-    class Music {
+        class Music
+        {
         public:
             Music(std::string);
-            Music(std::string, unsigned char*, int);
+            Music(std::string, unsigned char *, int);
             ~Music();
 
-            inline bool isMusicStreamPlaying(void)    const { return ::IsMusicStreamPlaying(this->_music);    };
-            inline void UpdateMusicStream(void)       const { ::UpdateMusicStream(this->_music);              };
+            inline bool isMusicStreamPlaying(void) const { return ::IsMusicStreamPlaying(this->_music); };
+            inline void UpdateMusicStream(void) const { ::UpdateMusicStream(this->_music); };
 
-            inline void PlayMusicStream(void)   const { ::PlayMusicStream(this->_music);  };
-            inline void StopMusicStream(void)   const { ::StopMusicStream(this->_music);  };
-            inline void PauseMusicStream(void)  const { ::PauseMusicStream(this->_music); };
+            inline void PlayMusicStream(void) const { ::PlayMusicStream(this->_music); };
+            inline void StopMusicStream(void) const { ::StopMusicStream(this->_music); };
+            inline void PauseMusicStream(void) const { ::PauseMusicStream(this->_music); };
             inline void ResumeMusicStream(void) const { ::ResumeMusicStream(this->_music); };
 
             inline void SetMusicVolume(float volume) { ::SetMusicVolume(this->_music, volume); };
-            inline void SetMusicPitch(float pitch)   { ::SetMusicPitch(this->_music, pitch);   };
+            inline void SetMusicPitch(float pitch) { ::SetMusicPitch(this->_music, pitch); };
 
             inline float GetMusicTimeLength(void) const { return ::GetMusicTimeLength(this->_music); };
             inline float GetMusicTimePlayed(void) const { return ::GetMusicTimePlayed(this->_music); };
+
         private:
             ::Music _music;
-    };
+        };
 
-    class AudioStream {
+        class AudioStream
+        {
         public:
             AudioStream(unsigned int, unsigned int, unsigned int);
             ~AudioStream();
@@ -77,17 +86,19 @@ namespace Audio {
             inline void UpdateAudioStream(const void *data, int samplesCount) const { ::UpdateAudioStream(this->_audiostream, data, samplesCount); };
 
             inline bool isAudioStreamProcessed(void) const { return ::IsAudioStreamProcessed(this->_audiostream); };
-            inline bool isAudioStreamPlaying(void)   const { return ::IsAudioStreamPlaying(this->_audiostream);   };
+            inline bool isAudioStreamPlaying(void) const { return ::IsAudioStreamPlaying(this->_audiostream); };
 
-            inline void PlayAudioStream(void)   const { ::PlayAudioStream(this->_audiostream);   };
-            inline void PauseAudioStream(void)  const { ::PauseAudioStream(this->_audiostream);  };
+            inline void PlayAudioStream(void) const { ::PlayAudioStream(this->_audiostream); };
+            inline void PauseAudioStream(void) const { ::PauseAudioStream(this->_audiostream); };
             inline void ResumeAudioStream(void) const { ::ResumeAudioStream(this->_audiostream); };
-            inline void StopAudioStream(void)   const { ::StopAudioStream(this->_audiostream);   };
+            inline void StopAudioStream(void) const { ::StopAudioStream(this->_audiostream); };
 
-            inline void SetAudioStreamVolume(float volume)        const { ::SetAudioStreamVolume(this->_audiostream, volume); };
-            inline void SetAudioStreamPitch(float pitch)          const { ::SetAudioStreamPitch(this->_audiostream, pitch);   };
-            inline void SetAudioStreamBufferSizeDefault(int size) const { ::SetAudioStreamBufferSizeDefault(size);            };
+            inline void SetAudioStreamVolume(float volume) const { ::SetAudioStreamVolume(this->_audiostream, volume); };
+            inline void SetAudioStreamPitch(float pitch) const { ::SetAudioStreamPitch(this->_audiostream, pitch); };
+            inline void SetAudioStreamBufferSizeDefault(int size) const { ::SetAudioStreamBufferSizeDefault(size); };
+
         private:
             ::AudioStream _audiostream;
-    };
-}
+        };
+    }; // namespace Audio
+} // namespace Raylib
