22507dd865235215096addc3cf6a1b44ab48a214
Raphael Mandica <raphael.mandica@epitech.eu>
2021-06-09 12:18:24 +0200

    [UPDATE] ModelRaylib.cpp/hpp

--- src/engine/Models/ModelRaylib.cpp
+++ src/engine/Models/ModelRaylib.cpp
@@ -14,16 +14,37 @@ Model::ModelRaylib::ModelRaylib(const std::string filename)
 
 void Model::ModelRaylib::DrawModel(Misc::Vector<3> position, float scale, Misc::Colors tint)
 {
-    ::DrawModel(this->_model, ::Vector3{position.getX(), position.getY(), position.getZ()}, scale, ::Color{tint.getA(), tint.getR(), tint.getG(), tint.getB()});
+    ::DrawModel(this->_model, ::Vector3{position.getX(), position.getY(), position.getZ()}, scale, ::Color{tint.getR(), tint.getG(), tint.getB(), tint.getA()});
 }
 
-void DrawModelEx(Misc::Vector<3> position, Misc::Vector<3> rotationAxis, float rotationAngle, Misc::Vector<3> scale, Misc::Colors tint)
+void Model::ModelRaylib::DrawModelEx(Misc::Vector<3> position, Misc::Vector<3> rotationAxis, float rotationAngle, Misc::Vector<3> scale, Misc::Colors tint)
 {
     ::DrawModelEx(this->_model, ::Vector3{position.getX(), position.getY(), position.getZ()}, 
-                    ::Vector3{rotationAxis.getX(), rotationAxis.getY(), rotationAxis.getZ()}, 
-                    rotationAngle, 
-                    ::Vector3{scale.getX(), scale.getY(), scale.getZ()}, ::Color{tint.getA(), 
-                    tint.getR(), tint.getG(), tint.getB()});
+                                ::Vector3{rotationAxis.getX(), rotationAxis.getY(), rotationAxis.getZ()}, 
+                                rotationAngle, 
+                                ::Vector3{scale.getX(), scale.getY(), scale.getZ()}, 
+                                ::Color{tint.getR(), tint.getG(), tint.getB(), tint.getA()});
+}
+
+void Model::ModelRaylib::DrawModelWires(Misc::Vector<3> position, float scale, Misc::Colors tint)
+{
+    ::DrawModelWires(this->_model, ::Vector3{position.getX(), position.getY(), position.getZ()}, 
+                                    scale,
+                                    ::Color{tint.getR(), tint.getG(), tint.getB(), tint.getA()});
+}
+
+void Model::ModelRaylib::DrawModelWiresEx(Misc::Vector<3> position, Misc::Vector<3> rotationAxis, float rotationAngle, Misc::Vector<3> scale, Misc::Colors tint)
+{
+    ::DrawModelWiresEx(this->_model, ::Vector3{position.getX(), position.getY(), position.getZ()},
+                                     ::Vector3{rotationAxis.getX(), rotationAxis.getY(), rotationAxis.getZ()},
+                                     rotationAngle,
+                                     ::Vector3{scale.getX(), scale.getY(), scale.getZ()},
+                                     ::Color{tint.getR(), tint.getG(), tint.getB(), tint.getA()});
+}
+
+void Model::ModelRaylib::DrawBoundingBox(Misc::Colors color)
+{
+    ::DrawBoundingBox(this->_BoundingBox, ::Color{color.getR(), color.getG(), color.getB(), color.getA()});
 }
 
 
--- src/engine/Models/ModelRaylib.hpp
+++ src/engine/Models/ModelRaylib.hpp
@@ -18,10 +18,14 @@ namespace Model {
             ModelRaylib(std::string filename);
             void DrawModel(Misc::Vector<3> position, float scale, Misc::Colors tint);
             void DrawModelEx(Misc::Vector<3> position, Misc::Vector<3> rotationAxis, float rotationAngle, Misc::Vector<3> scale, Misc::Colors tint);
+            void DrawModelWires(Misc::Vector<3> position, float scale, Misc::Colors tint);
+            void DrawModelWiresEx(Misc::Vector<3> position, Misc::Vector<3> rotationAxis, float rotationAngle, Misc::Vector<3> scale, Misc::Colors tint);
+            void DrawBoundingBox(Misc::Colors color);
             ~ModelRaylib();
 
         protected:
         private:
             ::Model _model;
+            ::BoundingBox _BoundingBox;
     };
 }
