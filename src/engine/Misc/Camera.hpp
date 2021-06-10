2d184cb02503e5beb0cda982d0949df6f04e0d2d
0xArKrant <contact@arkrant.us>
2021-06-10 20:14:57 +0200

    [ADD] 📝 Doxygen comments for Camera.hpp

--- src/engine/Misc/Camera.hpp
+++ src/engine/Misc/Camera.hpp
@@ -21,29 +21,29 @@ namespace Misc {
         public:
             Camera2D(const Misc::Vector<2>, const Misc::Vector<2>, float, float);
             Camera2D();
-            ~Camera2D() = default;
+            ~Camera2D() = default; /*!< Camera2D default destructor */
 
-            inline Misc::Vector<2> getOffset()    const { return this->_offset; };
-            inline Misc::Vector<2> getTarget()    const { return this->_target; };
-            inline float           getRotation()  const { return this->_rotation; };
-            inline float           getZoom()      const { return this->_zoom; };
+            inline Misc::Vector<2> getOffset()    const { return this->_offset; }; /*!< Offset value getter @return Misc::Vector<2> offset */
+            inline Misc::Vector<2> getTarget()    const { return this->_target; }; /*!< Target value getter @return Misc::Vector<2> target*/
+            inline float           getRotation()  const { return this->_rotation; }; /*!< Rotation value getter @return float rotation*/
+            inline float           getZoom()      const { return this->_zoom; }; /*!< Zoom value getter @return float zoom */
         private:
-            Misc::Vector<2> _offset;
-            Misc::Vector<2> _target;
-            float           _rotation;
-            float           _zoom;
+            Misc::Vector<2> _offset; /*!< Misc::Vector<2> offset value of Camera2D*/
+            Misc::Vector<2> _target; /*!< Misc::Vector<2> target value of Camera2D*/
+            float           _rotation; /*!< Float rotation value of Camera2D*/
+            float           _zoom; /*!< Float zoom value of Camera2D*/
     };
 
     class Camera3D {
         public:
             Camera3D(const Misc::Vector<3>, const Misc::Vector<3>, const Misc::Vector<3>, float, int);
             Camera3D();
-            ~Camera3D() = default;
+            ~Camera3D() = default; /*!< Camera3D default destructor */
         private:
-            Misc::Vector<3> _position;
-            Misc::Vector<3> _target;
-            Misc::Vector<3> _up;
-            float           _fovy;
-            int             _projection;
+            Misc::Vector<3> _position; /*!< Misc::Vector<3> position value of Camera3D*/
+            Misc::Vector<3> _target; /*!< Misc::Vector<3> target value of Camera3D*/
+            Misc::Vector<3> _up; /*!< Misc::Vector<3> up value of Camera3D*/
+            float           _fovy; /*!< Float fovy (y view) offset value of Camera3D*/
+            int             _projection; /*!< Int projection value of Camera3D*/
     };
 }
