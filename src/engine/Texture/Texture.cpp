550e0100c7ea59d7ff170dcc06d3c47a721f86c0
0xArKrant <contact@arkrant.us>
2021-06-10 14:28:13 +0200

    [UPDATED] ✨ Texture namespace + formatting + added to cmake

--- src/engine/Texture/Texture.cpp
+++ src/engine/Texture/Texture.cpp
@@ -7,71 +7,43 @@
 
 #include "Texture.hpp"
 
-Texture::Texture::Texture(const std::string &fileName)
-{
+Indie::Raylib::Texture::Texture::Texture(const std::string& fileName) {
     this->_texture = ::LoadTexture(fileName.c_str());
 }
 
-Texture::Texture::~Texture()
-{
+Indie::Raylib::Texture::Texture::~Texture() {
     ::UnloadTexture(this->_texture);
 }
 
-void Texture::Texture::DrawTexture(int posX, int posY, Misc::Colors tint)
-{
-    ::DrawTexture(this->_texture, posX, posY, ::Color {
-        tint.getR(),
-        tint.getG(),
-        tint.getB(),
-        tint.getA()
-    });
+void Indie::Raylib::Texture::Texture::DrawTexture(int posX, int posY, Misc::Colors tint) {
+    ::DrawTexture(
+        this->_texture,
+        posX,
+        posY,
+        ::Color { tint.getR(),tint.getG(),tint.getB(),tint.getA() }
+    );
 }
 
-void Texture::Texture::DrawTextureV(Misc::Vector<2> position, Misc::Colors tint)
-{
+void Indie::Raylib::Texture::Texture::DrawTextureV(Misc::Vector<2> position, Misc::Colors tint) {
     ::DrawTextureV(this->_texture,
         ::Vector2 { position.getX(), position.getY() },
-        ::Color {
-            tint.getR(),
-            tint.getG(),
-            tint.getB(),
-            tint.getA()
-        }
+        ::Color{tint.getR(),tint.getG(),tint.getB(),tint.getA() }
     );
 }
 
-void Texture::Texture::DrawTextureEx(Misc::Vector<2> position, float rotation, float scale, Misc::Colors tint)
-{
+void Indie::Raylib::Texture::Texture::DrawTextureEx(Misc::Vector<2> position, float rotation, float scale, Misc::Colors tint) {
     ::DrawTextureEx(this->_texture,
         ::Vector2 { position.getX(), position.getY() },
-        rotation, scale,
-        ::Color {
-            tint.getR(),
-            tint.getG(),
-            tint.getB(),
-            tint.getA()
-        }
+        rotation,
+        scale,
+        ::Color{tint.getR(),tint.getG(),tint.getB(),tint.getA() }
     );
 }
 
-void Texture::Texture::DrawTextureRec(Misc::Rectangle source, Misc::Vector<2> position, Misc::Colors tint)
-{
+void Indie::Raylib::Texture::Texture::DrawTextureRec(Misc::Rectangle source, Misc::Vector<2> position, Misc::Colors tint) {
     ::DrawTextureRec(this->_texture,
-        ::Rectangle {
-            source.getX(),
-            source.getY(),
-            source.getWidth(),
-            source.getHeight()
-        },
-        ::Vector2 {
-            position.getX(),
-            position.getY()
-        },
-        ::Color {
-            tint.getR(),
-            tint.getG(),
-            tint.getB(),
-            tint.getA()
-        }
+        ::Rectangle {source.getX(), source.getY(), source.getWidth(), source.getHeight() },
+        ::Vector2{position.getX(),position.getY() },
+        ::Color{ tint.getR(), tint.getG(), tint.getB(), tint.getA() }
     );
 }
--- src/engine/Texture/Texture.hpp
+++ src/engine/Texture/Texture.hpp
@@ -12,18 +12,21 @@
 #include "Vector.hpp"
 #include "RectangleVector.hpp"
 
-namespace Texture {
-    class Texture {
-        public:
-            Texture(const std::string &fileName);
-            ~Texture();
+namespace Indie {
+    namespace Raylib {
+        namespace Texture {
+            class Texture {
+                public:
+                    Texture(const std::string& fileName);
+                    ~Texture();
 
-            void DrawTexture(int posX, int posY, Misc::Colors tint);
-            void DrawTextureV(Misc::Vector<2> position, Misc::Colors tint);
-            void DrawTextureEx(Misc::Vector<2> position, float rotation, float scale, Misc::Colors tint);
-            void DrawTextureRec(Misc::Rectangle source, Misc::Vector<2> position, Misc::Colors tint);
-        protected:
-        private:
-            ::Texture2D _texture;
-    };
+                    void DrawTexture(int posX, int posY, Misc::Colors tint);
+                    void DrawTextureV(Misc::Vector<2> position, Misc::Colors tint);
+                    void DrawTextureEx(Misc::Vector<2> position, float rotation, float scale, Misc::Colors tint);
+                    void DrawTextureRec(Misc::Rectangle source, Misc::Vector<2> position, Misc::Colors tint);
+                private:
+                    ::Texture2D _texture;
+            };
+        }
+    }
 }
