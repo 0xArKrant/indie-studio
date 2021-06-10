c24c63f81bcda94dd0eeab51c54a3a08f7497dac
0xArKrant <contact@arkrant.us>
2021-06-10 14:23:15 +0200

    [UPDATED] ✨ Text namespace + formatting

--- src/engine/Text/Text.cpp
+++ src/engine/Text/Text.cpp
@@ -7,18 +7,15 @@
 
 #include "Text.hpp"
 
-Text::Text::Text()
-{
+Indie::Raylib::Text::Text::Text() {
     this->_font = ::GetFontDefault();
 }
 
-Text::Text::Text(const std::string &fileName)
-{
+Indie::Raylib::Text::Text::Text(const std::string& fileName) {
     this->_font = ::LoadFont(fileName.c_str());
 }
 
-Text::Text::Text(const std::string &fileName, int fontSize, int *fontChars, int charsCount)
-{
+Indie::Raylib::Text::Text::Text(const std::string& fileName, int fontSize, int* fontChars, int charsCount) {
     this->_font = ::LoadFontEx(
         fileName.c_str(),
         fontSize,
@@ -27,92 +24,83 @@ Text::Text::Text(const std::string &fileName, int fontSize, int *fontChars, int 
     );
 }
 
-Text::Text::~Text()
-{
+Indie::Raylib::Text::Text::~Text() {
     ::UnloadFont(this->_font);
 }
 
-void Text::Text::DrawText(const std::string &text, int posX, int posY, int fontSize, Misc::Colors color)
-{
+void Indie::Raylib::Text::Text::DrawText(const std::string& text, int posX, int posY, int fontSize, Misc::Colors color) {
     ::DrawText(
         text.c_str(),
         posX,
         posY,
         fontSize,
-        ::Color { color.getR(), color.getG(), color.getB(), color.getA() }
+        ::Color{ color.getR(), color.getG(), color.getB(), color.getA() }
     );
 }
 
-void Text::Text::DrawTextEx(const std::string &text, Misc::Vector<2> position, float fontSize, float spacing, Misc::Colors tint)
-{
+void Indie::Raylib::Text::Text::DrawTextEx(const std::string& text, Misc::Vector<2> position, float fontSize, float spacing, Misc::Colors tint) {
     ::DrawTextEx(
         this->_font,
         text.c_str(),
-        ::Vector2 { position.getX(), position.getY() },
+        ::Vector2{ position.getX(), position.getY() },
         fontSize,
         spacing,
-        ::Color { tint.getR(), tint.getG(), tint.getB(), tint.getA() }
+        ::Color{ tint.getR(), tint.getG(), tint.getB(), tint.getA() }
     );
 }
 
-void Text::Text::DrawTextRec(const std::string &text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint)
-{
+void Indie::Raylib::Text::Text::DrawTextRec(const std::string& text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint) {
     ::DrawTextRec(
         this->_font,
         text.c_str(),
-        ::Rectangle { rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight() },
+        ::Rectangle{ rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight() },
         fontSize,
         spacing,
         wordWrap,
-        ::Color { tint.getR(), tint.getG(), tint.getB(), tint.getA() }
+        ::Color{ tint.getR(), tint.getG(), tint.getB(), tint.getA() }
     );
 }
 
-void Text::Text::DrawTextRecEx(const std::string &text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint, int selectStart, int selectLength, Misc::Colors selectTint, Misc::Colors selectBackTint)
-{
+void Indie::Raylib::Text::Text::DrawTextRecEx(const std::string& text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint, int selectStart, int selectLength, Misc::Colors selectTint, Misc::Colors selectBackTint) {
     ::DrawTextRecEx(
         this->_font,
         text.c_str(),
-        ::Rectangle { rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight() },
+        ::Rectangle{ rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight() },
         fontSize,
         spacing,
         wordWrap,
-        ::Color { tint.getR(), tint.getG(), tint.getB(), tint.getA() },
+        ::Color{ tint.getR(), tint.getG(), tint.getB(), tint.getA() },
         selectStart,
         selectLength,
-        ::Color { selectTint.getR(), selectTint.getG(), selectTint.getB(), selectTint.getA() },
-        ::Color { selectBackTint.getR(), selectBackTint.getG(), selectBackTint.getB(), selectBackTint.getA() }
+        ::Color{ selectTint.getR(), selectTint.getG(), selectTint.getB(), selectTint.getA() },
+        ::Color{ selectBackTint.getR(), selectBackTint.getG(), selectBackTint.getB(), selectBackTint.getA() }
     );
 }
 
-void Text::Text::DrawTextCodepoint(int codepoint, Misc::Vector<2> position, float fontSize, Misc::Colors tint)
-{
+void Indie::Raylib::Text::Text::DrawTextCodepoint(int codepoint, Misc::Vector<2> position, float fontSize, Misc::Colors tint) {
     ::DrawTextCodepoint(
         this->_font,
         codepoint,
         ::Vector2 { position.getX(), position.getY() },
         fontSize,
-        ::Color { tint.getR(), tint.getG(), tint.getB(), tint.getA() }
+        ::Color{ tint.getR(), tint.getG(), tint.getB(), tint.getA() }
     );
 }
 
-int Text::Text::MeasureText(const std::string &text, int fontSize)
-{
+int Indie::Raylib::Text::Text::MeasureText(const std::string& text, int fontSize) {
     return ::MeasureText(
         text.c_str(),
         fontSize
     );
 }
 
-Misc::Vector<2> Text::Text::MeasureTextEx(const std::string &text, float fontSize, float spacing)
-{
+Misc::Vector<2> Indie::Raylib::Text::Text::MeasureTextEx(const std::string& text, float fontSize, float spacing) {
     ::Vector2 ray_vector = ::MeasureTextEx(this->_font, text.c_str(), fontSize, spacing);
-    Misc::Vector<2> vector = {ray_vector.x, ray_vector.y};
+    Misc::Vector<2> vector = { ray_vector.x, ray_vector.y };
     return vector;
 }
 
-int Text::Text::GetGlyphIndex(int codepoint)
-{
+int Indie::Raylib::Text::Text::GetGlyphIndex(int codepoint) {
     return ::GetGlyphIndex(
         this->_font,
         codepoint
--- src/engine/Text/Text.hpp
+++ src/engine/Text/Text.hpp
@@ -9,27 +9,31 @@
 
 #include "IText.hpp"
 
-namespace Text {
-    class Text : public IText {
-        public:
-            Text();
-            Text(const std::string &fileName);
-            Text(const std::string &fileName, int fontSize, int *fontChars, int charsCount);
-            ~Text();
+namespace Indie {
+    namespace Raylib {
+        namespace Text {
+            class Text : public IText {
+                public:
+                    Text();
+                    Text(const std::string& fileName);
+                    Text(const std::string& fileName, int fontSize, int* fontChars, int charsCount);
+                    ~Text();
 
-            // Text drawing functions
-            void DrawText(const std::string &text, int posX, int posY, int fontSize, Misc::Colors color);
-            void DrawTextEx(const std::string &text, Misc::Vector<2> position, float fontSize, float spacing, Misc::Colors tint);
-            void DrawTextRec(const std::string &text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint);
-            void DrawTextRecEx(const std::string &text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint,
-                                int selectStart, int selectLength, Misc::Colors selectTint, Misc::Colors selectBackTint);
-            void DrawTextCodepoint(int codepoint, Misc::Vector<2> position, float fontSize, Misc::Colors tint);
+                    // Text drawing functions
+                    void DrawText(const std::string& text, int posX, int posY, int fontSize, Misc::Colors color);
+                    void DrawTextEx(const std::string& text, Misc::Vector<2> position, float fontSize, float spacing, Misc::Colors tint);
+                    void DrawTextRec(const std::string& text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint);
+                    void DrawTextRecEx(const std::string& text, Misc::Rectangle rec, float fontSize, float spacing, bool wordWrap, Misc::Colors tint,
+                        int selectStart, int selectLength, Misc::Colors selectTint, Misc::Colors selectBackTint);
+                    void DrawTextCodepoint(int codepoint, Misc::Vector<2> position, float fontSize, Misc::Colors tint);
 
-            // Text misc. functions
-            int MeasureText(const std::string &text, int fontSize);
-            Misc::Vector<2> MeasureTextEx(const std::string &text, float fontSize, float spacing);
-            int GetGlyphIndex(int codepoint);
-        private:
-            ::Font _font;
-    };
+                    // Text misc. functions
+                    int MeasureText(const std::string& text, int fontSize);
+                    Misc::Vector<2> MeasureTextEx(const std::string& text, float fontSize, float spacing);
+                    int GetGlyphIndex(int codepoint);
+                private:
+                    ::Font _font;
+            };
+        }
+    }
 }
