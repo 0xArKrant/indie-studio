5a8d4e9bc63e73c930a208987240ec5f82b0d5e6
0xArKrant <contact@arkrant.us>
2021-06-11 03:56:02 +0200

    [ADD] 📝 Complete doxygen for Shape/Geometric/Polygon

--- src/engine/Shape/Geometric/Polygon.cpp
+++ src/engine/Shape/Geometric/Polygon.cpp
@@ -1,12 +1,27 @@
-/*
-** EPITECH PROJECT, 2021
-** indie-studio
-** File description:
-** Polygon
-*/
+/**
+ * @file Polygon.cpp
+ * @author 0xArKrant (contact@arkrant.us)
+ * @brief Encapsulation of all Shape/Polygon module from raylib
+ * @version 0.1
+ * @date 2021-06-11
+ *
+ * @copyright Copyright (c) 2021
+ *
+ * Indie-Studio
+ * Epitech Nancy 2021
+ */
 
 #include "Polygon.hpp"
 
+/**
+ * @brief Construct a new Indie:: Raylib:: Shape:: Polygon:: Polygon object
+ *
+ * @param center
+ * @param sides
+ * @param radius
+ * @param rotation
+ * @param values
+ */
 Indie::Raylib::Shape::Polygon::Polygon(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, const Misc::Colors& values) {
     this->_center = center;
     this->_sides = sides;
@@ -15,6 +30,10 @@ Indie::Raylib::Shape::Polygon::Polygon(const Misc::Vector<2> center, const int s
     this->_colors = values;
 }
 
+/**
+ * @brief Draw a regular polygon (Vector version)
+ *
+ */
 void Indie::Raylib::Shape::Polygon::DrawShape() {
     ::DrawPoly(
         ::Vector2{ this->_center.getX(), this->_center.getY() },
@@ -25,6 +44,15 @@ void Indie::Raylib::Shape::Polygon::DrawShape() {
     );
 }
 
+/**
+ * @brief Construct a new Indie:: Raylib:: Shape:: Polygon Lines:: Polygon Lines object
+ *
+ * @param center
+ * @param sides
+ * @param radius
+ * @param rotation
+ * @param values
+ */
 Indie::Raylib::Shape::PolygonLines::PolygonLines(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, const Misc::Colors& values) {
     this->_center = center;
     this->_sides = sides;
@@ -33,6 +61,10 @@ Indie::Raylib::Shape::PolygonLines::PolygonLines(const Misc::Vector<2> center, c
     this->_colors = values;
 }
 
+/**
+ * @brief Draw a polygon outline of n sides
+ *
+ */
 void Indie::Raylib::Shape::PolygonLines::DrawShape() {
     ::DrawPolyLines(
         ::Vector2{ this->_center.getX(), this->_center.getY() },
@@ -43,6 +75,16 @@ void Indie::Raylib::Shape::PolygonLines::DrawShape() {
     );
 }
 
+/**
+ * @brief Construct a new Indie:: Raylib:: Shape:: Polygon Lines Ex:: Polygon Lines Ex object
+ *
+ * @param center
+ * @param sides
+ * @param radius
+ * @param rotation
+ * @param linethick
+ * @param values
+ */
 Indie::Raylib::Shape::PolygonLinesEx::PolygonLinesEx(const Misc::Vector<2> center, const int sides, const float radius, const float rotation, const float linethick, const Misc::Colors& values) {
     this->_center = center;
     this->_sides = sides;
@@ -52,6 +94,10 @@ Indie::Raylib::Shape::PolygonLinesEx::PolygonLinesEx(const Misc::Vector<2> cente
     this->_colors = values;
 }
 
+/**
+ * @brief Draw a polygon outline of n sides extended
+ *
+ */
 void Indie::Raylib::Shape::PolygonLinesEx::DrawShape() {
     ::DrawPolyLinesEx(
         ::Vector2{ this->_center.getX(), this->_center.getY() },
