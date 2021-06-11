c030aa63723a9858dba6f24c9d0ebb7aeee344aa
Jules <jules.martin@epitech.eu>
2021-06-11 14:48:15 +0200

    [ADD] (add client)

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -6,11 +6,27 @@
 */
 
 #include "Client.hpp"
+enum { max_length = 1024 };
 
-Client::Client::Client()
+Client::Client::Client(char *argv1, char *argv2) : s(io_context, udp::endpoint(udp::v4(), 0)), resolver(io_context)
 {
+    this->endpoints = this->resolver.resolve(udp::v4(), argv1, argv2);
 }
 
 Client::Client::~Client()
 {
+}
+
+void Client::Client::send_to(const std::string &message)
+{
+    this->s.send_to(boost::asio::buffer(message), *this->endpoints.begin());
+}
+
+std::string Client::Client::receive_from()
+{
+    std::string buffer = NULL;
+
+    this->s.receive_from(boost::asio::buffer(buffer), sender_endpoint);
+
+    return (buffer);
 }
--- src/client/Client.hpp
+++ src/client/Client.hpp
@@ -6,14 +6,28 @@
 */
 
 #pragma once
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
 
-namespace Client {
-    class Client {
+using boost::asio::ip::udp;
+namespace Indie
+    namespace Client {
+        class Client {
         public:
             Client();
             ~Client();
+            void send_to();
+            std::string receive_from();
 
         protected:
         private:
-    };
+            boost::asio::io_context io_context;
+            udp::socket s;
+            udp::resolver resolver;
+            udp::resolver::results_type endpoints;
+            udp::endpoint sender_endpoint;
+        };
+    }
 }
