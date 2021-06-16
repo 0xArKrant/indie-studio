3d8c0244830ca98f5f9c7b526e5ee715b7a48ad7
0xArKrant <contact@arkrant.us>
2021-06-16 17:55:51 +0200

    [UPDATE] SceneManager uniqueptr + Added SplashScreen class

--- src/core/IScene.hpp
+++ src/core/IScene.hpp
@@ -14,7 +14,7 @@ namespace Indie {
         class IScene         {
             public:
                 ~IScene() = default;
-                virtual void update(std::stack<IScene *>&) = 0;
+                virtual void update(std::stack<std::unique_ptr<IScene>>&) = 0;
                 virtual void draw() = 0;
 
             protected:
--- src/core/SceneManagement.cpp
+++ src/core/SceneManagement.cpp
@@ -8,14 +8,14 @@
 #include "SceneManagement.hpp"
 #include "Colors.hpp"
 
-Indie::Core::SceneManagement::SceneManagement(int width, int height, std::string &name) 
+Indie::Core::SceneManagement::SceneManagement(int width, int height, std::string &name)
 {
     (void) width;
     (void) height;
     (void) name;
     // this->_window = Indie::Raylib::Core::Core::getInstance(width, height, name);
-    Indie::Scene::MainMenu *menu = new Indie::Scene::MainMenu();
-    this->_stack.push(menu);
+    std::unique_ptr<Indie::Scene::SplashScreen> scene = std::make_unique<Indie::Scene::SplashScreen>();
+    this->_stack.push(std::move(scene));
 }
 
 Indie::Core::SceneManagement::~SceneManagement() {
