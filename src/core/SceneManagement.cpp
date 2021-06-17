964c9810b482f3afbb51b8608f3ae4a9940dbb6c
Raphael Mandica <raphael.mandica@epitech.eu>
2021-06-17 17:06:06 +0200

    [add] scenemanagement.cpp

---
+++ src/core/SceneManagement.cpp
@@ -0,0 +1,26 @@
+#include "SceneManagement.hpp"
+#include "Colors.hpp"
+
+Indie::Core::SceneManagement::SceneManagement(int width, int height, std::string &name)
+{
+    (void) width;
+    (void) height;
+    (void) name;
+    // this->_window = Indie::Raylib::Core::Core::getInstance(width, height, name);
+    std::unique_ptr<Indie::Scene::SplashScreen> scene = std::make_unique<Indie::Scene::SplashScreen>();
+    this->_stack.push(std::move(scene));
+}
+
+Indie::Core::SceneManagement::~SceneManagement() {
+}
+
+void Indie::Core::SceneManagement::loop() {
+    while (!this->_window.WindowShouldClose()) {
+        this->_stack.top()->update(this->_stack);
+        this->_window.BeginDrawing();
+        this->_window.ClearBackground(Misc::Colors(0, 0, 0, 0));
+        // Ajout de tick //
+        this->_stack.top()->draw();
+        this->_window.EndDrawing();
+    }
+}
