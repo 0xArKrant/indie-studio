dfd4b90dc2dc731a9bd8109083ce19f3b160a81e
0xArKrant <0xArKrant@users.noreply.github.com>
2021-06-18 17:04:07 +0000

    deploy: d4cc2d0ac869c9040f4dec40fe8c1d502aae0e05

--- search/all_2.js
+++ search/all_2.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['begindrawing_132',['BeginDrawing',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#a219c7f2e2f2ddaa2300025ca81e44692',1,'Indie::Raylib::Core::Core']]],
-  ['beginmode2d_133',['BeginMode2D',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#a06b5446dad4d386a9841dd8f3cb64274',1,'Indie::Raylib::Core::Core']]],
-  ['beginmode3d_134',['BeginMode3D',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#a2c67ec985fb2dd5190601e79377c9457',1,'Indie::Raylib::Core::Core']]],
+  ['beginmode2d_133',['BeginMode2D',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#aa20854a4b1717c6cf62d5e8dfa64b81f',1,'Indie::Raylib::Core::Core']]],
+  ['beginmode3d_134',['BeginMode3D',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#a1ecbcffcac1a22f35705adbdadf648e1',1,'Indie::Raylib::Core::Core']]],
   ['begintexturemode_135',['BeginTextureMode',['../d5/d65/classIndie_1_1Raylib_1_1Core_1_1Core.html#af992ca7cba446d2d5cd0b69c31548709',1,'Indie::Raylib::Core::Core']]],
   ['bomb_136',['BOMB',['../d1/de7/classIndie_1_1Game_1_1GameObject.html#a0437f2b288b41616620752adae8d981fa4596300e74600f484b9a29d7ed48863d',1,'Indie::Game::GameObject']]],
   ['bomberman_2ecpp_137',['Bomberman.cpp',['../de/d11/Bomberman_8cpp.html',1,'']]],
