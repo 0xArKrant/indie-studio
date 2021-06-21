7f9f513347cd42b76808c971fe5a5cf8c954bce0
0xArKrant <contact@arkrant.us>
2021-06-21 12:53:02 +0200

    [REMOVED] Server-rest-api (won't use)

--- src/server-rest-api/config/mongo.js
+++
@@ -1,5 +0,0 @@
-dbPassword = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_COLLECTIONS}?retryWrites=true&w=majority`;
-
-module.exports = {
-    mongoURI: dbPassword,
-};
