b150de712ffccda2b1aea1473bbc411e1f3980a8
Raphael Mandica <raphael.mandica@epitech.eu>
2021-06-22 13:14:18 +0200

    [add] Idleanimation

--- src/core/Player.cpp
+++ src/core/Player.cpp
@@ -17,7 +17,8 @@ Indie::Game::Player::Player(const std::string &objPath, const std::string &id, M
 }
 
 Indie::Game::Player::Player(const std::string &objPath, const std::string &texturePath, const std::string &id, Misc::Vector<3> pos, bool display) : Model3D(objPath, texturePath, id, pos, display),
-_anim("./assets/Muhammer/MuhammerAnim", "./assets/Muhammer/Muhammer.png")
+_anim("./assets/Muhammer/MuhammerAnim", "./assets/Muhammer/Muhammer.png"),
+_animIdle("./assets/Muhammer/MuhammerIdle", "./assets/Muhammer/Muhammer.png")
 {
     this->_speed = 2;
     this->_rota = {0.0f, 1.0f, 0.0f};
@@ -36,21 +37,25 @@ void Indie::Game::Player::update(float elapsedTimes)
         this->_rotaAngle = 270.0f;
         this->_pos.move((-this->_speed * elapsedTimes), 0.0f, 0.0f);
         this->_anim.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
+        this->_animIdle.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
     }
     if (Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_A) || Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_LEFT)) {
         this->_rotaAngle = 0.0f;
         this->_pos.move(0.0f, 0.0f, (this->_speed * elapsedTimes));
         this->_anim.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
+        this->_animIdle.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
     }
     if (Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_S) || Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_DOWN)) {
         this->_rotaAngle = 90.0f;
         this->_pos.move((this->_speed * elapsedTimes), 0.0f, 0.0f);
         this->_anim.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
+        this->_animIdle.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
     }
     if (Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_D) || Indie::Raylib::Core::Core::getInstance().getInputKeyboard().IsKeyDown(KEY_RIGHT)) {
         this->_rotaAngle = 180.0f;
         this->_pos.move(0.0f, 0.0f, (-this->_speed * elapsedTimes));
         this->_anim.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
+        this->_animIdle.update(elapsedTimes, this->_pos, this->_rotaAngle, this->_scale, this->_rota);
     }
 }
 
@@ -58,5 +63,6 @@ void Indie::Game::Player::draw()
 {
     //this->_model.DrawModel(this->_pos, 10.0f, Misc::Colors(255, 255, 255, 255));
     this->_anim.draw();
+    this->_animIdle.draw();
     //this->_model.DrawModelEx(this->_pos, this->_rota, this->_rotaAngle,  this->_scale, Misc::Colors(130, 130, 130, 255));
 }
--- src/core/Player.hpp
+++ src/core/Player.hpp
@@ -28,6 +28,7 @@ namespace Indie {
                 Misc::Vector<3> _scale;
                 float _rotaAngle;
                 Indie::Raylib::AnimesRaylib _anim;
+                Indie::Raylib::AnimesRaylib _animIdle;
                 //std::vector<Model3D> _bombs;
             private:
         };
